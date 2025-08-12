e2e2a8969524c5612e264117d739cc691118ce45
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-08-12 15:23:24 +0900

    Title: 달에서 무게 구하기, Time: 83ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250812/달에서 무게 구하기/weight-on-the-moon.java
+++ 250812/달에서 무게 구하기/weight-on-the-moon.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         int weight = 13;
-        double ratio = 0.165;
+        double ratio = 0.165000;
         System.out.println(weight+" * "+ ratio+" = "+(weight*ratio));
     }
 }
